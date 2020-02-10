3eabbf0dd7ada25cd401e808e9e6551c6f2737f2
wilkuukliw <44411707+wilkuukliw@users.noreply.github.com>
2020-02-10 16:20:49 +0100

    Add files via upload
    initial commit

---
+++ Mandatory1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mandatory1
+//
+//  Created by Anna Maria on 07/02/2020.
+//  Copyright © 2020 Anna Maria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mandatory1/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  Mandatory1
+//
+//  Created by Anna Maria on 07/02/2020.
+//  Copyright © 2020 Anna Maria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
