3a6c5177bc9b3a255009b34aa5ef9704d42f3c09
wilkuukliw <44411707+wilkuukliw@users.noreply.github.com>
2020-03-19 18:10:52 +0100

    Add files via upload
    week 10 hand in - upload files to Firebase

---
+++ Week 10/Notebook/Notebook/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Notebook
+//
+//  Created by Anna Maria on 03/03/2020.
+//  Copyright © 2020 Anna Maria. All rights reserved.
+//
+
+import UIKit
+import FirebaseStorage
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    let imagePicker = UIImagePickerController()
+    var images = [String]()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var btnUpload: UIButton!
+    @IBOutlet weak var btnGallery: UIButton!
+    
+    var picker = UIImagePickerController()
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        imagePicker.delegate = self
+        picker.delegate = self
+        
+    }
+        
+    @IBAction func galleryPressed(_ sender: AnyObject) {
+        print("Gallery")
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+            
+    }
+    
+    @IBAction func pickImageBtnClicked(_ sender: Any) {
+     imagePicker.allowsEditing = false
+     imagePicker.sourceType = .photoLibrary
+         
+     present(imagePicker, animated: true, completion: nil)
+     
+    }
+        
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+            CloudStorage.uploadImage(image: pickedImage)
+            
+            }
+        
+            dismiss(animated: true, completion: nil)
+        
+    }
+            
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            dismiss(animated: true, completion: nil)
+            }
+    
+}
